c056403eaab79d96d089fe9c4f70d946b27de8ca
Lucas Gomes <107519714+euolucasgomes@users.noreply.github.com>
2026-04-23 21:24:35 -0300

    feat(project): initialize Spring Boot project structure (#5)
    Co-authored-by: Lucas Gomes <lucasgomes.dados@gmail.com>

---
+++ src/main/java/com/lucasgomes/auth_access_service/AuthAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lucasgomes.auth_access_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAccessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAccessServiceApplication.class, args);
+	}
+
+}

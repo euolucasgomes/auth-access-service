a9d691e317f1940cee2c25987a8ca049d36880d4
Lucas Gomes <fluxovendas7@gmail.com>
2026-04-23 21:21:03 -0300

    feat(project): initialize Spring Boot project structure

---
+++ src/test/java/com/lucasgomes/auth_access_service/AuthAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasgomes.auth_access_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAccessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

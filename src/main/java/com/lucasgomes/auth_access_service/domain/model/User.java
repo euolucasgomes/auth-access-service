ac63c6515b3fbf5409f292443830ec753e1f3f53
Lucas Gomes <lucasgomes.dados@gmail.com>
2026-04-28 23:23:08 -0300

    feat(domain): add validations to User entity

--- src/main/java/com/lucasgomes/auth_access_service/domain/model/User.java
+++ src/main/java/com/lucasgomes/auth_access_service/domain/model/User.java
@@ -12,12 +12,37 @@ public class User {
     private boolean active;
 
     public User(UUID id, String name, String email, String passwordHash, UserRole role, boolean active) {
+        validate(name, email, passwordHash, role);
+
         this.id = id;
         this.name = name;
         this.email = email;
         this.passwordHash = passwordHash;
         this.role = role;
         this.active = active;
+    }
+
+    private void validate(String name, String email, String passwordHash, UserRole role) {
+
+        if (isBlank(name)) {
+            throw new IllegalArgumentException("Name cannot be null or blank");
+        }
+
+        if (isBlank(email)) {
+            throw new IllegalArgumentException("Email cannot be null or blank");
+        }
+
+        if (isBlank(passwordHash)) {
+            throw new IllegalArgumentException("Password hash cannot be null or blank");
+        }
+
+        if (role == null) {
+            throw new IllegalArgumentException("User role cannot be null");
+        }
+    }
+
+    private boolean isBlank(String value) {
+        return value == null || value.trim().isEmpty();
     }
 
     public UUID getId() {
